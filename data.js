50fec389d35d1e9cb7b336dd4c435aff473aa467
Ansly21 <anslybrandon.dy.iics@ust.edu.ph>
2023-06-30 09:34:03 +0800

    Fixed data

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
-        price: 14,
+        price: 15,
         img: "images/pizza.jpg",
         alt: "Pizza from vecteezy.com",
         id: 0
@@ -18,6 +18,7 @@ export const menuArray = [
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
+        price: 10,
         img: "images/beer.jpg",
         alt: "Beer from vecteezy.com",
         id: 2
